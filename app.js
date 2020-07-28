53efb11f2f818f589ae1e7f2ebf74b1e8ebf8698
SandeepX <sandeeppant024@gmail.com>
2020-07-28 11:26:30 +0545

    simple punch game  using Vue Js

---
+++ app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#Punchgame',
+
+  data: {
+  	Title:'This is a simple puchgame',
+    health:100,
+    ended:false
+  },
+
+  methods:{
+
+  	punch:function(){
+  		this.health-=10;
+  		if(this.health<=0){
+  			this.ended=true;
+  		}
+  	},
+
+  	restart:function(){
+  		this.health=100;
+  		this.ended=false;
+  	}
+
+  },
+
+  computed:{
+
+  }
+
+});
